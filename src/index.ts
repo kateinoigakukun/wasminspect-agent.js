2c11da944e10cb7daeb0bc99e7521cdb7a6967d2
Yuta Saito <kateinoigakukun@gmail.com>
2021-04-06 10:27:58 +0900

    remove init-memory call

--- src/index.ts
+++ src/index.ts
@@ -194,8 +194,6 @@ export namespace WasmInspect {
     await worker.receive("OnSocketOpen");
     rpc.binaryRequest(uint8Buffer);
     const init = await rpc.textResponse("Init");
-    rpc.textRequest({ type: "InitMemory" });
-    const initMemory = await rpc.binaryResponse(BinaryResponseKind.InitMemory);
     return new Module(init, rpc, worker);
   }
 
